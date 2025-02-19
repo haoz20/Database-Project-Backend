34cdca0c792d0c59e64ad7e7e0083072b85ea528
haoz20 <zhaonianhao2005@gmail.com>
2025-02-19 12:47:03 +0700

    Initial Project Setup

---
+++ src/main/java/org/example/database_project_backend/DatabaseProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.database_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/database_project_backend/DatabaseProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.database_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
